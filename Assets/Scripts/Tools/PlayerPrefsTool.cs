cd8132e6f5cb12bdbabda79b6d8a2389994a6b2a
TeamanZz <a.yasinko@mail.ru>
2022-01-06 19:19:40 +0300

    Clean up

---
+++ Assets/Scripts/Tools/PlayerPrefsTool.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerPrefsTool : MonoBehaviour
+{
+#if UNITY_EDITOR
+    [MenuItem("PlayerPrefs/Clear Player Prefs")]
+    static void ClearPlayerPrefs()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+    // [MenuItem("PlayerPrefs/Add10000PP")]
+
+    // static void Add10000PP()
+    // {
+    //     var oldValue = PlayerPrefs.GetInt("PopulationPoints");
+
+    //     oldValue += 10000;
+    //     PlayerPrefs.SetInt("PopulationPoints", oldValue);
+    // }
+#endif
+}
